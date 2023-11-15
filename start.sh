1ce9555ad04814502216c46af4ac7e30b494719a
Vadym <vpylypenko@corevalue.net>
2023-11-15 18:54:27 +0200

    Demo local env

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+REPOSITORY=true
+ENVIRONMENT=main
+GIT_URL_USER_SERVICE=git@github.com:vadpylypenko/ms-demo-user.git
+GIT_URL_VM_SERVICE=git@github.com:vadpylypenko/ms-demo-vm.git
+
+
+function LOAD_REPO_DATA() {
+    echo "INFO: COPY_REPOSITORY - Start"
+
+    if ! [ -d "ms-demo-user" ]; then
+           echo "INFO: CLONE_REPOSITORY USER SERVICE - Start"
+           git clone $GIT_URL_USER_SERVICE
+           cd ms-demo-user
+           git checkout $ENVIRONMENT
+           cd ..   
+    fi
+    if ! [ -d "ms-demo-vm" ]; then
+           echo "INFO: CLONE_REPOSITORY VM SERVICE - Start"
+           git clone $GIT_URL_VM_SERVICE
+           cd ms-demo-vm
+           git checkout $ENVIRONMENT
+           cd ..    
+    fi
+}
+
+if [[ "$REPOSITORY" = true ]]; then
+    LOAD_REPO_DATA
+fi
